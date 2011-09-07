5dc90262c7b90583005b86b264748e2fc6e6fd65
Stan Rozenraukh <stan@stanistan.com>
2011-09-07 12:06:14 -0300

    Remove no longer used code.

--- lib/class.J.php
+++ lib/class.J.php
@@ -16,34 +16,10 @@ class J {
 		$fn();
 	}
 
-	public static function it($note, $fn) {
-		self::$last_it = $note;
-		$fn();
-	}
 
 	public static function expect($fn) {
 		if (is_callable($fn)) $fn = $fn();
 		return new Expect($fn, self::$last_it);
-	}
-
-	public static function result($message = null, $status, $expected = null, $given = null) {
-		if ($status) {
-			print_r('Successfull test: '.$message);
-		} else {
-			print_r('Test Failed: '.$message);
-			if ($expected != $given) {
-				$expected = J::valToString($expected);
-				$given = J::valToString($given);
-				print_r("\n--[$expected] Expected. Result was [$given]");
-			}
-		}
-		print_r("\n");
-	}
-
-	public static function valToString($val) {
-		if (is_string($val)) return $val;
-		if (is_null($val)) return 'NULL';
-		if (is_object($val)) return var_export($val, true);
 	}
 
 }
